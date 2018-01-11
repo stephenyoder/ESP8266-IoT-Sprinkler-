06bf219a753779891df258f3481527c57f6a25ab
stephenyoder <stephenayoder@gmail.com>
2018-01-11 14:04:45 -0800

    Add files via upload

---
+++ DHT11.ino
@@ -0,0 +1,111 @@
+// Lab 2 Exercise 1
+// Temp/Humidity Sensor
+/*
+#include <dht11.h>   // Library for DHT11 Temperature/Humidity Sensor
+
+dht11 DHT11;
+
+#define DHT11PIN 14   // Set Data Pin from Sensor (Pin D5)
+
+void setup()
+{
+  Serial.begin(115200);   // Set Serial Transfer Rate
+  Serial.println("DHT11 TEST PROGRAM ");
+  Serial.println();
+}
+
+void loop()
+{
+  Serial.println("\n");   // New Line
+
+  int chk = DHT11.read(DHT11PIN);   // Read Data Pin
+
+  Serial.print("Read Sensor: ");
+  
+  switch (chk)    // Handle Errors
+  {
+    case DHTLIB_OK: 
+    Serial.println("OK"); 
+    break;
+    case DHTLIB_ERROR_CHECKSUM: 
+    Serial.println("Checksum error"); 
+    break;
+    case DHTLIB_ERROR_TIMEOUT: 
+    Serial.println("Time out error"); 
+    break;
+    default: 
+    Serial.println("Unknown error"); 
+    break;
+  }
+
+  // Print Humidity to Serial Monitor
+  Serial.print("Humidity: ");
+  Serial.print(String(DHT11.humidity));
+  Serial.println("%");
+
+  // Print Temperature to Serial Monitor
+  Serial.print("Temperature: ");
+  Serial.print(String(DHT11.temperature));
+  Serial.println("C");
+
+  // Wait 2000ms
+  delay(2000);
+}*/
+//
+// END OF FILE
+//
+
+/*
+#include "DHT11.h"
+#define dht_apin 14 // Analog Pin sensor is connected to
+ 
+DHT11 DHT;
+ 
+void setup(){
+ 
+  Serial.begin(9600);
+  delay(500);//Delay to let system boot
+  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  delay(1000);//Wait before accessing Sensor
+ 
+}//end "setup()"
+ 
+void loop(){
+  //Start of Program 
+ 
+    DHT.setup(dht_apin);
+    
+    Serial.print("Current humidity = ");
+    Serial.print(DHT.getHumidity());
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(DHT.getTemperature()); 
+    Serial.println("C  ");
+    
+    delay(5000);//Wait 5 seconds before accessing sensor again.
+ 
+  //Fastest should be once every two seconds.
+ 
+}// end loop() */
+
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 14 
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.print(DHT.temperature);
+  Serial.print("C  ");
+  Serial.print("Humidity = ");
+  Serial.print(DHT.humidity);
+  Serial.println("%  ");
+  delay(1000);
+}
---
+++ DHT11wLCD.ino
@@ -0,0 +1,73 @@
+// Lab 2 Exercise 2
+// Temperature/Humidity on OLED Module
+
+#include <dht.h>    // Library for DHT11 Temperature/Humidity Sensor
+
+// Libraries for the Oled LCD LED Module
+#include <Wire.h> 
+#include <ACROBOTIC_SSD1306.h>
+
+dht DHT;
+
+#define DHT11_PIN 14   // Set Data Pin from Sensor
+
+// Initialization
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();  
+  oled.init();                      // Initialze SSD1306 OLED display
+  oled.clearDisplay();              // Clear screen
+  oled.setTextXY(0,0);              // Set cursor position, start of line 0
+  oled.putString("Read Sensor: ");
+  oled.setTextXY(2,0);              // Set cursor position, start of line 2
+  oled.putString("Humidity: ");
+  oled.setTextXY(4,0);              // Set cursor position, start of line 4
+  oled.putString("Temperature: ");
+}
+
+// Program Loop
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);   // Read Data Pin
+  
+  /*switch (chk)    // Handle Errors
+  {
+    case "OK": 
+    oled.setTextXY(0,12);             
+    oled.putString("OK");
+    break;
+    case "CHECKSUM": 
+    oled.setTextXY(0,0);              
+    oled.putString("Checksum error"); 
+    break;
+    case "TIMEOUT": 
+    oled.setTextXY(0,0);              
+    oled.putString("Time out error");
+    break;
+    default: 
+    oled.setTextXY(0,0);              
+    oled.putString("Unknown error"); 
+    break;
+  }*/
+
+  // Print Humidity
+  oled.setTextXY(2,9);    
+  //float humidity = DHT.humidity;          
+  oled.putString((String) DHT.humidity);
+  oled.setTextXY(2,11);              
+  oled.putString("%  ");
+
+  // Print Temperature
+  oled.setTextXY(4,12);      
+  //float temperature = DHT.temperature;        
+  oled.putString((String) DHT.temperature);
+  oled.setTextXY(4,14);              
+  oled.putString("C  ");
+
+  // Wait 2000ms
+  delay(2000);
+}
+//
+// END OF FILE
+//
