066a7f09957e3ea94f0ac9b019c151c3aa713ade
stephenyoder <stephenayoder@gmail.com>
2018-01-11 22:39:20 -0800

    Add files via upload

---
+++ WiFi_Connect.ino
@@ -0,0 +1,32 @@
+// Connecting to WiFi
+
+#include <ESP8266WiFi.h>
+
+// Enter WiFi Details
+const char* ssid = "Point-D";
+const char* password = "";
+
+void setup() {
+  
+  Serial.begin(115200);
+  delay(2000);
+  
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi Connected");
+  Serial.println("IP Address: ");
+  Serial.println(WiFi.localIP());   // Print IP Address of the board
+}
+
+// Infinite Loop
+void loop() {
+  }
